ec2033c3fcc4ca71d4e2fa312c9f8a631f46e6a6
sohail <sohailkhaninsta@gmail.com>
2022-03-01 12:42:22 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Checkout from './Checkout';
+import Login from './Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Switch>
+        <Route path="/login">
+          <Login/>
+        </Route>
+      <Route path="/checkout">
+        <Checkout/>
+      </Route>
+      <Route path="/">
+      <Header/>
+        <Home/>
+      </Route>
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
